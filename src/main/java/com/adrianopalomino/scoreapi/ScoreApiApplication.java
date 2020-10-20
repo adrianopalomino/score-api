28fb09d08938734194a1bed893ab6f7d8d3a536d
adriano.dantas <adriano.dantas@ctis.com.br>
2020-10-20 20:05:46 -0300

    Definição de arquitetura e dependencias básicas / Inicio do projeto

---
+++ src/main/java/com/adrianopalomino/scoreapi/ScoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.adrianopalomino.scoreapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScoreApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adrianopalomino/scoreapi/ScoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adrianopalomino.scoreapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
