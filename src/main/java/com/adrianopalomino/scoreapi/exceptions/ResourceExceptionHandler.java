d0c4957567047871b001592bc340950ee9edc34d
adriano.dantas <adriano.dantas@ctis.com.br>
2020-10-21 00:06:43 -0300

    Criação de handler de exceptions

---
+++ src/main/java/com/adrianopalomino/scoreapi/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.adrianopalomino.scoreapi.exceptions;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
+		StandardError err = new StandardError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(), "Não encontrado", e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+	}
+	
+	@ExceptionHandler(MissingServletRequestParameterException.class)
+	public ResponseEntity<StandardError> handleMissingParams(MissingServletRequestParameterException ex, HttpServletRequest request) {
+		StringBuilder str = new StringBuilder();
+		str.append("O parâmetro ").append(ex.getParameterName()).append(" é obrigatório");
+		StandardError err = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), ex.getMessage(), str.toString(), request.getRequestURI());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	}
+	
+}
