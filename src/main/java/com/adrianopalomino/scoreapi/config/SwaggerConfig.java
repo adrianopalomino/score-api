50b6a49680b26cddf12b8e87f7b424068674d61d
adriano.dantas <adriano.dantas@ctis.com.br>
2020-10-21 00:06:00 -0300

    Configuração swagger

---
+++ src/main/java/com/adrianopalomino/scoreapi/config/SwaggerConfig.java
@@ -0,0 +1,47 @@
+package com.adrianopalomino.scoreapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig extends WebMvcConfigurationSupport {
+
+  @Bean
+  public Docket greetingApi() {
+    return new Docket(DocumentationType.SWAGGER_2)
+        .select()
+        .apis(RequestHandlerSelectors.basePackage("com.adrianopalomino.scoreapi"))
+        .build()
+        .apiInfo(metaData());
+
+  }
+
+  private ApiInfo metaData() {
+    return new ApiInfoBuilder()
+        .title("Spring Boot REST API")
+        .description("\"Spring Boot REST API para análise de crédito\"")
+        .version("1.0.0")
+        .license("Apache License Version 2.0")
+        .licenseUrl("https://apache.org/licenses/LICENSE-2.0")
+        .build();
+  }
+
+  @Override
+  protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+    registry.addResourceHandler("swagger-ui.html")
+        .addResourceLocations("classpath:/META-INF/resources/");
+
+    registry.addResourceHandler("/webjars/**")
+        .addResourceLocations("classpath:/META-INF/resources/webjars/");
+  }
+}
