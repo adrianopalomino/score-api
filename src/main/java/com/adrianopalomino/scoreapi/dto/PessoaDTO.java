33f32a0cc4bbc418f22743d7312b0ca8ad4f88a4
adriano.dantas <adriano.dantas@ctis.com.br>
2020-10-23 15:17:46 -0300

    Incluindo mecanismo de validação de campos

--- src/main/java/com/adrianopalomino/scoreapi/dto/PessoaDTO.java
+++ src/main/java/com/adrianopalomino/scoreapi/dto/PessoaDTO.java
@@ -3,19 +3,31 @@ package com.adrianopalomino.scoreapi.dto;
 import java.io.Serializable;
 import java.math.BigDecimal;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import io.swagger.annotations.ApiModelProperty;
 
 public class PessoaDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@ApiModelProperty(value = "Nome da pessoa")
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Length(min=5, max=80, message="O tamanho deve ser entre 2 e 80 caracteres")
 	private String nome;
+	
 	@ApiModelProperty(value = "Idade da pessoa")
 	private int idade;
+	
 	@ApiModelProperty(value = "CPF da pessoa")
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Length(min=11, max=11, message="O tamanho deve ser de 11 caracteres")
 	private String cpf;
+	
 	@ApiModelProperty(value = "Quantidade de dependentes da pessoa")
 	private int dependentes;
+	
 	@ApiModelProperty(value = "Renda da pessoa")
 	private BigDecimal renda;
 
--- src/main/java/com/adrianopalomino/scoreapi/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/adrianopalomino/scoreapi/exceptions/ResourceExceptionHandler.java
@@ -4,6 +4,8 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -23,6 +25,16 @@ public class ResourceExceptionHandler {
 		str.append("O parâmetro ").append(ex.getParameterName()).append(" é obrigatório");
 		StandardError err = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), ex.getMessage(), str.toString(), request.getRequestURI());
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandardError> validation(MethodArgumentNotValidException e, HttpServletRequest request) {
+		
+		ValidationError err = new ValidationError(System.currentTimeMillis(), HttpStatus.UNPROCESSABLE_ENTITY.value(), "Erro de validação", e.getMessage(), request.getRequestURI());
+		for (FieldError x : e.getBindingResult().getFieldErrors()) {
+			err.addError(x.getField(), x.getDefaultMessage());
+		}		
+		return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(err);
 	}
 	
 }
